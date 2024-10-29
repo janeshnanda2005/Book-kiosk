833f956e4b632513e7de5c37567249fcb6162fc5
Janeshnanda K S <officialjanesh12@gmail.com>
2024-10-29 13:58:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
 import datetime
-import json
+import firebase_admin
+from firebase_admin import credentials, db
 
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate('C:\Vignesh\main\Book-kiosk\cred.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://book-kiosk-93f35-default-rtdb.europe-west1.firebasedatabase.app'
+})
 
 API_KEY = 'AIzaSyARlrYxIKVkTUQLXA796cQjxWZXcx-BHFI'
 
@@ -21,30 +27,40 @@ def main(isbn):
     else:
         return "Failed to retrieve data."
 
-
 def write_book():
-    
-    db = {}
     counter = 1
     while True:
         print("Hello there Drop Your Book in Box!!")
         isbn_book1 = input("Scan Your Book :")
         book1 = main(isbn_book1)
+        
+        if book1 in ["No book found with this ISBN.", "Failed to retrieve data."]:
+            continue
+        
+        
         print(f"Book Title: {book1}")
         print("Done!!")
-        isbn_book2= input("Scan Your New Book :")
+        
+        
+        isbn_book2 = input("Scan Your New Book :")
         val_1 = datetime.datetime.now().isoformat()
         book2 = main(isbn_book2)
+        
+        
         print(f"Book Title: {book2}")
         print("Do visit us again!!")
+        
         data = {
-            'ID':f'{counter}',
-            'book_1':f'{book1}',
-            'book_2':f'{book2}',
-            'date_time':f'{val_1}'
+            'ID': f'{counter}',
+            'book_1': f'{book1}',
+            'book_2': f'{book2}',
+            'date_time': f'{val_1}'
         }
-        db[counter] = data
-        with open('dataisbn.json','w') as file:
-            json.dump(db,file,indent=4,separators=(", "," : "))
-        counter+=1
-write_book()
+        
+
+        ref = db.reference('books')
+        ref.push(data)
+        
+        counter += 1
+
+write_boo
